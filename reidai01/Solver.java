55c72b2b0f3a2fcc8c69486b96d3a817871c2614
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-06-24 02:24:18 +0900

    nn + 2-opt score : 890

--- reidai01/Solver.java
+++ reidai01/Solver.java
@@ -14,28 +14,40 @@ public class Solver {
         long start = System.currentTimeMillis();
         long end;
 
-        for (int i = 0; i < size; i++) x[i] = i;
+        //for (int i = 0; i < size; i++) x[i] = i;
 
         Solver.minDistance = Solver.getCurrentDistance();
 
-        for (int yaju = 810; yaju < 114514; yaju++) {
-            int s = Solver.minDistance;
+        nearestNeighbour();
 
-            for (int i = size - 1; i >= 1; i--) {
+        twoOptimization();
+        /*
+
+        while(true){
+            int s = Solver.minDistance;
+            for (int i = 1; i < size; i++) {
                 for (int j = i + 1; j < size; j++) {
-                    reverse(i, j);
+                    int tmp = x[j];
+                    for (int k = j - 1; k >= i; k--) x[k + 1] = x[k];
+                    x[i] = tmp;
+
                     int cur = Solver.getCurrentDistance();
                     if (Solver.minDistance > cur) {
                         Solver.minDistance = cur;
                     } else {
-                        reverse(i, j);
+                        tmp = x[i];
+                        for (int k = i; k < j; k++) x[k] = x[k + 1];
+                        x[j] = tmp;
                     }
                 }
             }
-            if (s == Solver.minDistance) break;
-            System.err.println(yaju + "th try");
-            print();
-        }
+            if(s == Solver.minDistance) break;
+            end = System.currentTimeMillis();
+            if (end - start > TLE - MARGIN) break;
+        }*/
+
+        /*
+
         Random rand = new Random();
 
         while (true) {
@@ -57,12 +69,59 @@ public class Solver {
             }
             end = System.currentTimeMillis();
             if (end - start > TLE - MARGIN) break;
-        }
+        }*/
 
         System.err.println("final try");
         print();
 
         TSP2D.submit(x);
+    }
+
+    private static void nearestNeighbour() {
+        boolean[] used = new boolean[size];
+        for (int i = 0; i < size; i++) used[i] = false;
+        used[0] = true;
+        int from = 0;
+        for (int i = 1; i < size; i++) {
+            int dist = 1145141919;
+            int distPos = 0;
+            for (int to = 1; to < size; to++) {
+                if (from == to) continue;
+                if(used[to]) continue;
+
+                if (dist > TSP2D.distance(from, to)) {
+                    dist = TSP2D.distance(from, to);
+                    distPos = to;
+                }
+            }
+
+            x[i] = distPos;
+            from = distPos;
+            used[distPos] = true;
+        }
+    }
+
+    private static void twoOptimization() {
+        Solver.minDistance = Solver.getCurrentDistance();
+
+        for (int yaju = 810; yaju < 114514; yaju++) {
+            int s = Solver.minDistance;
+
+            for (int i = size - 1; i >= 1; i--) {
+                for (int j = i + 1; j < size; j++) {
+                    reverse(i, j);
+                    int cur = Solver.getCurrentDistance();
+                    if (Solver.minDistance > cur) {
+                        Solver.minDistance = cur;
+                    } else {
+                        reverse(i, j);
+                    }
+                }
+            }
+            if (s == Solver.minDistance) break;
+            System.err.println(yaju + "th try");
+            print();
+        }
     }
 
     private static int getCurrentDistance() {
